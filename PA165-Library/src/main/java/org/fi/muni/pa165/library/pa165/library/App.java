3bb4e35a32897547aad4294eea0b61690978dbed
bloodspell <bloodspell@Thinkpad>
2013-10-08 18:59:14 +0200

    cisty maven projekt

---
+++ PA165-Library/src/main/java/org/fi/muni/pa165/library/pa165/library/App.java
@@ -0,0 +1,13 @@
+package org.fi.muni.pa165.library.pa165.library;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
