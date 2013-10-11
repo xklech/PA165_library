87c2b47b4a4a5437f15603f80a14f5c75cad86d2
IvanaHaraslinova <haraslinova.ivana@gmail.com>
2013-10-11 19:35:47 +0200

    Update CustomerTest.java

--- src/test/java/cz/fi/muni/pa165/library/CustomerTest.java
+++ src/test/java/cz/fi/muni/pa165/library/CustomerTest.java
@@ -38,22 +38,48 @@ public class CustomerTest {
     
     @Test
     public void testAddCustomer() throws Exception {
-
+	Customer customer = new Customer((new Long(2350), "John", "Smith", "1 New Oxford Street, London", new Date(23-06-1983), "830623/6973", null);
+        Customer customerSaved;
+        em.getTransaction().begin();
+        customerDAO.addCustomer(customer);
+        em.getTransaction().commit();
+        customerSaved = em.find(Customer.class, customer.getId());
+        Assert.assertEquals(customer, customerSaved);
     }
     
     @Test
     public void testUpdateCustomer() throws Exception {
+        Customer customer = em.find(Customer.class,new Long(1));
+        customer.setFirstName("George");
+        em.getTransaction().begin();
+        customerDAO.updateCustomer(customer);
+        em.getTransaction().commit();
+        em.clear();
+        Customer customer2 = em.find(Customer.class,new Long(1));
+        Assert.assertEquals("George",customer2.getFirstName());
 
     }
     
     @Test
     public void testDeleteCustomer() throws Exception {
+    	Customer customer = em.find(Customer.class,new Long(1));
+        em.getTransaction().begin();
+        customerDAO.deleteCustomer(customer);
+        em.getTransaction().commit();
+        em.clear();
+        Customer customer2 = em.find(Customer.class,new Long(1));
+        Assert.assertNull(customer2);
 
     }
     
     @Test
     public void testFindCustomerById() throws Exception {
-
+	Customer customer = em.find(Customer.class,new Long(2));
+        Customer customer2 = customerDAO.findCustomerById(new Long(2));
+        Assert.assertEquals(customer, customer2);
+        
+        Customer customer3 = customerDAO.findCustomerById(new Long(754));
+        Assert.assertNull(customer3);
     }
     
     @Test
