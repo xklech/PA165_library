090ca1a21a6feed51bef4b41ea7ad4669f945083
xklech <jarek.klech@seznam.cz>
2013-10-30 18:49:00 +0100

    directory change

--- src/main/java/cz/fi/muni/pa165/library/dao/BookDAOImpl.java
+++ src/main/java/cz/fi/muni/pa165/library/dao/BookDAOImpl.java
@@ -8,6 +8,7 @@ import java.util.Date;
 import java.util.List;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.PersistenceContext;
 import javax.persistence.PersistenceUnit;
 import javax.persistence.TypedQuery;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -17,19 +18,13 @@ import org.springframework.beans.factory.annotation.Autowired;
  *
  * @author Jaroslav Klech
  */
+
+
 public class BookDaoImpl implements BookDao {
 
-    @Autowired
-    private EntityManagerFactory entityManagerFactory;
-    @PersistenceUnit
-    public void setEntityManagerFactory(EntityManagerFactory emf) {
-        this.entityManagerFactory = emf;
-    }
+    @PersistenceContext(unitName = "LibraryPU")
+    private EntityManager entityManager;
 
-
-    public EntityManagerFactory getEmf() {
-        return entityManagerFactory;
-    }
 
     
     
@@ -42,22 +37,21 @@ public class BookDaoImpl implements BookDao {
             throw new BookDaoException("addBook: given book has filled id");
         }
         System.err.println("dao:addbook called");
-        EntityManager em = entityManagerFactory.createEntityManager();
-        em.persist(book);
-        em.flush();
-        em.close();
+
+        entityManager.persist(book);
+
     }
 
     @Override
     public void updateBook(Book book) throws BookDaoException {
-        EntityManager em = entityManagerFactory.createEntityManager();
+
         if (book == null) {
             throw new BookDaoException("updateBook: given book atribute is null");
         }
         if (book.getId() == null) {
             throw new BookDaoException("updateBook: given book has not filled id");
         }
-        Book savedBook = em.find(Book.class, book.getId());
+        Book savedBook = entityManager.find(Book.class, book.getId());
         if (savedBook == null) {
             throw new BookDaoException("updateBook: given book doesn't exist in DB");
         }
@@ -67,101 +61,91 @@ public class BookDaoImpl implements BookDao {
         savedBook.setImpressions(book.getImpressions());
         savedBook.setName(book.getName());
         savedBook.setPublishDate(book.getPublishDate());
-        em.close();
+
     }
 
     @Override
     public void deleteBook(Book book) throws BookDaoException {
-        EntityManager em = entityManagerFactory.createEntityManager();
         if (book == null) {
             throw new BookDaoException("deleteBook: given book atribute is null");
         }
         if (book.getId() == null) {
             throw new BookDaoException("deleteBook: given book has not filled id");
         }
-        Book savedBook = em.find(Book.class, book.getId());
+        Book savedBook = entityManager.find(Book.class, book.getId());
         if (savedBook == null) {
             throw new BookDaoException("deleteBook: given book doesn't exist in DB");
         }
-        em.remove(savedBook);
-        em.close();
+        entityManager.remove(savedBook);
+
     }
 
     @Override
     public Book findBookById(Long id) throws BookDaoException {
-        EntityManager em = entityManagerFactory.createEntityManager();
         if (id == null) {
             throw new BookDaoException("findBookById: given attribute id is null");
         }
-        Book book = em.find(Book.class, id);
-        em.close();
+        Book book = entityManager.find(Book.class, id);
+
         return book;
     }
 
     @Override
     public Book findBookByISBN(String isbn) {
-        EntityManager em = entityManagerFactory.createEntityManager();
-        TypedQuery query = em.createQuery("SELECT b FROM Book b WHERE b.isbn = :isbn", Book.class);
+
+        TypedQuery query = entityManager.createQuery("SELECT b FROM Book b WHERE b.isbn = :isbn", Book.class);
         query.setParameter("isbn", isbn);
         List<Book> books = query.getResultList();
         if (books == null || books.isEmpty()) {
             return null;
         }
-        em.close();
+
         return books.get(0);
 
     }
 
     @Override
     public Collection<Book> findBooksByAuthor(String author) {
-        EntityManager em = entityManagerFactory.createEntityManager();
-        TypedQuery query = em.createQuery("SELECT b FROM Book b WHERE b.author LIKE :author", Book.class);
+        TypedQuery query = entityManager.createQuery("SELECT b FROM Book b WHERE b.author LIKE :author", Book.class);
         query.setParameter("author", "%" + author + "%");
-        em.close();
         return query.getResultList();
     }
 
     @Override
     public Collection<Book> findBooksByPublishDate(Date publishDateFrom, Date publishDateTo) {
-        EntityManager em = entityManagerFactory.createEntityManager();
-        TypedQuery query = em.createQuery("SELECT b FROM Book b WHERE b.publishDate > :publishDateFrom AND b.publishDate < :publishDateTo", Book.class);
+        TypedQuery query = entityManager.createQuery("SELECT b FROM Book b WHERE b.publishDate > :publishDateFrom AND b.publishDate < :publishDateTo", Book.class);
         query.setParameter("publishDateFrom", publishDateFrom);
         query.setParameter("publishDateTo", publishDateTo);
-        em.close();
         return query.getResultList();
     }
 
     @Override
     public Collection<Book> findBooksByName(String name) {
-        EntityManager em = entityManagerFactory.createEntityManager();
-        TypedQuery query = em.createQuery("SELECT b FROM Book b WHERE b.name LIKE :name", Book.class);
+        TypedQuery query = entityManager.createQuery("SELECT b FROM Book b WHERE b.name LIKE :name", Book.class);
         query.setParameter("name", "%" + name + "%");
         List<Book>list = query.getResultList();
-        em.close();
+        entityManager.close();
         return list;
     }
 
     @Override
     public Collection<Book> findBooksByDepartment(String department) {
-        EntityManager em = entityManagerFactory.createEntityManager();
-        TypedQuery query = em.createQuery("SELECT b FROM Book b WHERE b.department LIKE :department", Book.class);
+        TypedQuery query = entityManager.createQuery("SELECT b FROM Book b WHERE b.department LIKE :department", Book.class);
         query.setParameter("department", "%" + department + "%");
-        em.close();
         return query.getResultList();
     }
 
     @Override
     public Book findBookByImpression(Impression impression) throws BookDaoException {
-        EntityManager em = entityManagerFactory.createEntityManager();
         if (impression == null) {
             throw new BookDaoException("findBookByImpression: given impression atribute is null");
         }
         if (impression.getId() == null) {
             throw new BookDaoException("findBookByImpression: impressions id is null");
         }
-        TypedQuery query = em.createQuery("SELECT b FROM Book b LEFT JOIN b.impressions i WHERE i.id = :id", Book.class);
+        TypedQuery query = entityManager.createQuery("SELECT b FROM Book b LEFT JOIN b.impressions i WHERE i.id = :id", Book.class);
         query.setParameter("id", impression.getId());
-        em.close();
+
         return (Book) query.getSingleResult();
     }
 
